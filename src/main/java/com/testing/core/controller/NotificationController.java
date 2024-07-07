307343fd903af96cff7c4344ea43f1d35c2b179d
mazimpakarichard <47664816+mazimpakarichard@users.noreply.github.com>
2024-07-07 16:46:57 +0200

    sending emails with attachment testing (#27)
    * sending emails with attachment testing
    
    * basepachage check
    
    * notification controller
    
    ---------
    
    Co-authored-by: Richard Mazimpaka <richard.mazimpaka@risa.gov.rw>

--- src/main/java/com/testing/core/controller/NotificationController.java
+++ src/main/java/com/testing/core/controller/NotificationController.java
@@ -1,14 +1,20 @@
 package com.testing.core.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import reactor.core.publisher.Mono;
+import rw.eccellenza.core.notification.domain.EmailRequest;
 import rw.eccellenza.core.notification.domain.EmailResponse;
+import rw.eccellenza.core.notification.domain.EmailUtil;
 import rw.eccellenza.core.notification.domain.MailList;
 import rw.eccellenza.core.notification.service.IEmailService;
 
@@ -22,11 +28,31 @@ public class NotificationController {
 
   @Autowired private IEmailService emailService;
 
-  @PostMapping("/email/send")
+  @PostMapping(
+      path = "/email/send",
+      consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
   public ResponseEntity<Mono<EmailResponse>> sendEmail(
-      @RequestBody(required = true) MailList mailList) {
+      @RequestPart(name = "emailRequest", required = true) EmailRequest emailRequest,
+      @RequestPart("attachment") MultipartFile attachment) {
 
     try {
+
+      log.info("Received file with content type: {}", attachment.getContentType());
+
+      MailList mailList = new MailList();
+      EmailUtil emailUtil = new EmailUtil();
+      List<EmailUtil> emailUtils = new ArrayList<>();
+      emailUtil.setSubject(emailRequest.getSubject());
+      emailUtil.setBodyText(emailRequest.getBodyText());
+      emailUtil.setReceipient(emailRequest.getReceipient());
+
+      emailUtil.setContentType(attachment.getContentType());
+      emailUtil.setMyBytes(attachment.getBytes());
+      emailUtil.setFilename(attachment.getName());
+
+      emailUtils.add(emailUtil);
+      mailList.setEmails(emailUtils);
+
       log.info("Initiating email sending process for: {}", mailList);
 
       Mono<EmailResponse> responseMono =
