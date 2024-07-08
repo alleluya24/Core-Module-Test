fabd1ebec1a3dcf83a5fa38d5951c5649bb6b91f
Richard Mazimpaka <richard.mazimpaka@risa.gov.rw>
2024-07-08 15:25:21 +0200

    minor change

--- src/main/java/com/testing/core/controller/NotificationController.java
+++ src/main/java/com/testing/core/controller/NotificationController.java
@@ -28,7 +28,9 @@ public class NotificationController {
 
   @Autowired private IEmailService emailService;
 
-  @PostMapping(path = "/email/send")
+  @PostMapping(
+      path = "/email/send",
+      consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
   public ResponseEntity<Mono<EmailResponse>> sendEmail(
       @RequestPart(name = "emailRequest", required = true) EmailRequest emailRequest,
       @RequestPart("attachment") MultipartFile attachment) {
