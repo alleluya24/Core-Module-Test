4d324e2a0101458eb0aa0f458a3c5c9f50856803
Ramba <daddiramba@gmail.com>
2024-10-01 01:27:40 +0200

    added test of survey general

--- src/main/java/com/testing/core/controller/SurveyorGeneralController.java
+++ src/main/java/com/testing/core/controller/SurveyorGeneralController.java
@@ -57,14 +57,12 @@ public class SurveyorGeneralController {
     }
   }
 
-  @GetMapping("/seach-property/{districtId}/{townshipId}/{searchKey}")
+  @GetMapping("/seach-property/{searchKey}")
   public ResponseEntity<PropertyResponse> searchProperty(
-      @PathVariable("districtId") String districtId,
-      @PathVariable("townshipId") String townshipId,
       @PathVariable("searchKey") String searchKey) {
     try {
       return new ResponseEntity<>(
-          surveyGeneralIntegrationService.searchProperty(districtId, townshipId, searchKey).block(),
+          surveyGeneralIntegrationService.searchProperty(searchKey).block(),
           HttpStatus.OK);
     } catch (Exception e) {
       log.error(e.getMessage(), e);
